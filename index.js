2fcc4f45a775af58dd61e5ff2a8cf25929b7c17d
Deadwoodxbruh <aakash.gudivada@gmail.com>
2026-02-03 18:20:36 +0530

    updated object to string

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ app.get("/response",async (req,res) =>{
 
 app.post("/post", async (req, res) => {
     const body = req.body;
-    const parm = req.query;
-    const { message } = body.message || parm.message;
+    const query = req.query;
+    const message = body.message || query.message;
     if (!message){return res.status(400).json({"success":false,"messsage":"Invalid message input."})};
     try {
         await axios.post(`https://discord.com/api/webhooks/1353624030535745607/aoeQkFGv4j2kliZ9nswY-Hs75hFbHsCDX7pnXvaZG9XvtosnMabjjxJbD6hNhnh5duLD`, 
