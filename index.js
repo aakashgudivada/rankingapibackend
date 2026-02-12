6ff8af7a94cd967c6c2a2e92921cc09b17ea59b6
Deadwoodxbruh <aakash.gudivada@gmail.com>
2026-02-12 10:43:44 +0530

    Updated batch

--- index.js
+++ index.js
@@ -1,181 +1,128 @@
-require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const groq = require("groq-sdk");
-
-const aiag = new groq.Groq({
-    apiKey: process.env.apikeygroq
-})
-
-const apikey = process.env.dbkey;
-
-const {Redis} = require("@upstash/redis");
-
-const client = new Redis({
-    url: "https://calm-barnacle-35826.upstash.io",
-    token: "AYvyAAIncDEzNWU0MzY2ZWEzY2U0Y2UzOWQ0YmE0OTRkMmYzOTY2NXAxMzU4MjY"
-})
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(helmet());
-app.use(cors());
-
-// Parse incoming JSON requests
-app.use(express.json());
-
-app.get("/",(req,res)=>{
-    res.status(200).json({"success":true,"timestamp":Date.now()})
-})
-
-const defaultmsg = "You are speaking on behalf of Aakash (Woodx) from woodx.onrender.com. Only answer questions related to his expertise; for off-topic queries, politely state they are unrelated. Woodx’s background: 7 years Lua, 5 years JavaScript/Node.js/HTML/CSS/React/Angular, 50+ public full-stack APIs handling 100K+ monthly requests, creator of the Roblox game 'Nearly Impossible Difficulty Chart Obby' with 19M+ visits, age 19, fluent in English, Telugu, and French. Only share this information professionally when asked. If unsure, respond that you do not know and suggest contacting directly. Always respond professionally in an American tone. Only mention explicitly provided info; do not add extra suggestions or assumptions. These are overall top projects of mine, for recent projects tell them to check the website projects page."
-
-app.get("/response",async (req,res) =>{
-    const query = req.query;
-    const body = req.body;
-    const defaultmessage = body.default || defaultmsg;
-
-    let message = query.message || body.message || null;
-    if (message === null){
-        res.status(400).json({"success":false})
-        return;
+const defaultPrompt = ""
+function getResponse(prompt){
+    if (prompt){
+        try{
+            // const response = await fetch("")
+        }catch(error){
+            console.log(error)
+        }
     }
-    try{
-        const response1 = await aiag.chat.completions.create({
-            "messages": [{
-                role: "user",
-                content: message
-            },
-            {
-                role: "system",
-                content: defaultmessage
+    return;
+}
+
+
+const classes = ["widgets"];
+const elementsData = {
+    "htitle": {"Type":"h1","UE":"None"},
+    "utitle": {"Type":"p","UE":"None"},
+    "nbutton": {"Type":"button","UE":"None"},
+    "widgets": {"Type":"section","UE":"None"},
+    "widget-img": {"Type":"img","UE":"widgets"},
+    "widget": {"Type":"section","UE":"widgets"},
+    "widget-p": {"Type":"p","UE":"widget"},
+    "widget-h": {"Type":"h3","UE":"widget"},
+    "tags": {"Type":"ul","UE":"None"},
+    "time": {"Type":"p","UE":"tags"},
+    "genre": {"Type":"p","UE":"tags"},
+    "title": {"Type":"h1","UE":"None"},
+    "desc": {"Type":"p","UE":"None"},
+    "cc": {"Type":"a","UE":"None"},
+    "uc": {"Type":"h2","UE":"None"},
+    "gap": {"Type":"li","UE":"None"},
+    "subtitle": {"Type":"p","UE":"None"}
+};
+const app = document.querySelector(".app");
+
+function render(dictelements){
+    if (dictelements){
+        const map = {};
+        dictelements.forEach((item) =>{
+            const config = elementsData[item["type"]]
+            if (!config) return;
+            const el = document.createElement(config.Type);
+            el.id = item.type || "";
+            if (item.content) el.textContent = item.content;
+            if (item.src) el.src = item.src;
+            if (item.href) el.href = item.href;
+            if (item.alt) el.alt = item.alt;
+            map[item.Type] = el;
+            if (config.UE === "None"){
+                app.appendChild(el);
+            }else{
+                const parent = map[config.UE];
+                parent.appendChild(el)
             }
-        ],
-            "model": "openai/gpt-oss-120b"
         })
-        if (response1 && response1.choices){
-            res.status(200).json({"success":true,"message":response1.choices[0].message.content})
+    }
+}
+render([
+    {
+        "type": "htitle",
+        "content": "Hi, I'm an Artificial Intelligence & Game Developer."
+    },
+    {
+        "type": "utitle",
+        "content": "You can explore my popular services, projects and open-source publications here! Please do not republish or repost as their as subjective to copyright and patent reserved."
+    },
+    {
+        "type": "title",
+        "content": "You can exido notght and patent reserved."
+    },
+    {
+        "type": "desc",
+        "content": "You can explore my popular services, projects and open-source publications here! Please do not republish or repost as their as subjective to copyright and patent reserved."
+    }
+])
+
+document.addEventListener("DOMContentLoaded",function(){
+    fetch("https://rankingapibackend.onrender.com/")
+    .then((data) =>{
+        return data.json()
+    })
+    .then((data) => {
+        if (data.success === true){
+            console.log("Loaded website successfully!")
+        }
+    })
+    const sidebarbutton = document.getElementById("sidebar");
+    let toggled = false;
+
+    const prompt = document.querySelector(".prompt");
+    const promptinput = prompt.querySelector("input");
+    const promptButton = prompt.querySelector("img");
+
+    promptButton.addEventListener("click",async function(event){
+        console.log("uh")
+        event.preventDefault();
+        const usermessage = promptinput.value;
+        const request = await groq.chat.completions.create({
+            "messages": [
+              {
+                "role": "user",
+                "content": usermessage
+              }
+            ],
+            "model": "openai/gpt-oss-120b",
+            "temperature": 1,
+            "max_completion_tokens": 8192,
+            "top_p": 1,
+            "stream": true,
+            "reasoning_effort": "medium",
+            "stop": null
+          });
+          console.log(request)
+    })
+
+    sidebarbutton.addEventListener('click', function() {
+        toggled = !toggled;
+        document.querySelector('.selection').classList.toggle('open');
+        document.querySelector(".app").classList.toggle("focus");
+        prompt.classList.toggle("focus")
+        if (toggled){
+            sidebarbutton.src = "src/imgs/sidebar2.png"
         }else{
-            res.status(400).json({"success":false})
+            sidebarbutton.src = "src/imgs/sidebar.png"
         }
-    }catch(error){
-        res.status(400).json({"success":false});
-        console.log(error)
-    }
-    return
+        return;
+    });
 })
-
-app.post("/post", async (req, res) => {
-    const body = req.body;
-    const query = req.query;
-    const messageinput = body.message || query.message;
-    if (!messageinput){return res.status(400).json({"success":false,"messsage":"Invalid message input."})};
-    try {
-        console.log(messageinput);
-        const response = await fetch("https://discord.com/api/webhooks/1353624030535745607/aoeQkFGv4j2kliZ9nswY-Hs75hFbHsCDX7pnXvaZG9XvtosnMabjjxJbD6hNhnh5duLD",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({content: messageinput})
-        }
-        )
-        .then(() =>{
-            res.status(200).json({ "success": true });
-        })
-    } catch (error) {
-        console.error("Discord error:", {
-            status: error.response?.status,
-            data: error.response?.data,
-            message: error.message
-        });
-        res.status(500).json({ success: false, error: error.response?.data || "Internal error" });
-    }
-});
-
-app.post("/save",async(req,res) =>{
-    const query = req.query;
-    const body = req.body;
-    const key = body.key;
-    if (!body.key){res.status(400).json("Auth failed!")};
-    if (key === apikey){
-        const providedkey = query.key;
-        const value = query.value;
-        if (value && providedkey){
-            await client.set(providedkey,value)
-            res.status(200).json({"success":true,"message":"Saved the data!"})
-        }else{
-            res.status(400).json({"success":false,"message":"Key and value not provided."})
-        }
-    }else{
-        res.status(400).json({"success":false,"message":"Auth failed!"})
-    }
-    return;
-})
-
-app.get("/get",async(req,res) =>{
-    const query = req.query;
-    const body = req.body;
-    if (!body.key){res.status(400).json("Auth failed!")}
-    if (body.key == apikey){
-        const key = query.key;
-        if (key){
-            const value = await client.get(key) || "none";
-            res.status(200).json({"success":true,"value":value})
-        }else{
-            res.status(400).json({"success":false})
-        }
-    }else{
-        res.status(400)
-    }
-    return;
-})
-
-app.get("/del",async(req,res) =>{
-    const query = req.query;
-    const body = req.body;
-    if (!body.key){res.status(400).json("Auth failed!")}
-    if (body.key == apikey){
-        const key = query.key;
-        if (key){
-            await client.del(key);
-            res.status(200).json({"success":true,"message":"key deleted"})
-        }else{
-            res.status(400).json({"success":false})
-        }
-    }else{
-        res.status(400)
-    }
-    return;
-})
-
-app.get('/roles', async (req, res) => {
-    const query = req.query;
-    const id = query.groupid;
-    if (!id){
-        res.status(400).json({"error":"Please provide a groupId"})
-    }
-    try{
-        const response = await fetch(`https://groups.roblox.com/v1/groups/${id}/roles`);
-        if (!response.ok){
-            res.json(response.status).json({"error":"Failed to fetch from API."});
-        }
-        const data = await response.json();
-        res.json(data)
-    }catch(error){
-        console.log(error)
-    }
-});
-
-// 3. Handle 404 (Route not found)
-app.use((req, res) => {
-  res.status(404).json({ error: 'Route not found' });
-});
-
-
-// --- Start Server ---
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
